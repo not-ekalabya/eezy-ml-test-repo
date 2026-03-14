935515206bf7090dbd12527a8fac4f29a443106b
unknown <ekalabya2010@gmail.com>
2026-03-14 23:49:31 +0530

    real-time logging

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 
 @app.route("/health", methods=["GET"])
 def health():
-    return jsonify({"status": "ok - auto deployment works <test 2>!"})
+    return jsonify({"status": "ok - real-time logging!"})
 
 
 @app.route("/test", methods=["GET"])
